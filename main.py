d1cab2e243e729563c9f0c2a6e8fa8747c3d14d9
Raja Chellappa <rajachellappa@gmail.com>
2022-02-05 07:40:08 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Import modules
+import pandas as pd
+import numpy as np
+
+
+# Read in dataset
+df = pd.read_csv('datasets/bank_data.csv')
+
+# Print the first few rows of the DataFrame
+df.head()
